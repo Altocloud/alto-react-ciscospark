ecf357d76be17d906023020e09f14ebcde72906d
Colm Seale <colm.seale@altocloud.com>
2017-03-03 17:46:17 +0000

    builing altocloud spark guest outh component

--- src/altocloud-demo-widget/index.js
+++ src/altocloud-demo-widget/index.js
@@ -7,7 +7,6 @@ import autobind from 'autobind-decorator';
 import SparkLogo from '@ciscospark/react-component-spark-logo';
 import SparkOAuth from '@ciscospark/react-component-spark-oauth';
 import WidgetMessageMeet from '@altocloud/widget';
-import ExampleCode, {MODE_REACT, MODE_INLINE} from '../example-code';
 
 import styles from './styles.css';
 
@@ -21,9 +20,7 @@ class DemoWidgetMessageMeet extends Component {
     const clientSecret = process.env.MESSAGE_DEMO_CLIENT_SECRET;
     this.state = {
       authenticate: false,
-      mode: MODE_INLINE,
       accessToken: cookie.load(`accessToken`) || ``,
-      toPersonEmail: cookie.load(`toPersonEmail`) || ``,
       running: false,
       clientId,
       clientSecret,
@@ -39,24 +36,10 @@ class DemoWidgetMessageMeet extends Component {
   @autobind
   handleSubmit(e) {
     e.preventDefault();
-    cookie.save(`accessToken`, this.state.accessToken);
-    cookie.save(`toPersonEmail`, this.state.toPersonEmail);
+    // get Free User Information
+    // cookie.save(`accessToken`, this.state.accessToken);
+    // cookie.save(`toPersonEmail`, this.state.toPersonEmail);
     this.setState({running: true});
-  }
-
-  @autobind
-  handleAccessTokenChange(e) {
-    return this.setState({accessToken: e.target.value});
-  }
-
-  @autobind
-  handleEmailChange(e) {
-    return this.setState({toPersonEmail: e.target.value});
-  }
-
-  @autobind
-  handleModeChange(e) {
-    return this.setState({mode: e.target.value});
   }
 
   @autobind
@@ -89,73 +72,13 @@ class DemoWidgetMessageMeet extends Component {
           <SparkLogo />
         </div>
         <form className={classNames(`demo-form`, styles.demoForm)}>
-          {`Altocloud Cisco Spark Demo Example`}
-          <div className={classNames(`field-wrapper`, styles.fieldWrapper)}>
-            <input
-              className={classNames(`field-input`, styles.fieldInput)}
-              onChange={this.handleEmailChange}
-              placeholder="To User Email"
-              type="text"
-              value={this.state.toPersonEmail}
-            />
-          </div>
-          <div className={classNames(`field-wrapper`, styles.fieldWrapper)}>
-            <input
-              className={classNames(`field-input`, styles.fieldInput)}
-              onChange={this.handleAccessTokenChange}
-              placeholder="Your Access Token"
-              type="text"
-              value={this.state.accessToken}
-            />
-          </div>
-          <div className={classNames(`field-wrapper`, styles.fieldWrapper)}>
-            <button
-              className={classNames(`button-small`, styles.buttonSmall)}
-              onClick={this.handleLoginOAuth}
-            >
-              {`Login with Spark`}
-            </button>
-          </div>
-          <div className={classNames(`field-wrapper`, styles.fieldWrapper)}>
-            <a href="http://developer.ciscospark.com">{`Get access token from developer.ciscospark.com`}</a>
-          </div>
-          <div className={classNames(`field-wrapper`, styles.fieldWrapper)}>
-            <div className={classNames(`radio-group`, styles.radioGroup)}>
-              <div className={classNames(`radio-item`, styles.radioItem)}>
-                <input
-                  checked={this.state.mode === MODE_INLINE}
-                  id="radio_inline"
-                  onChange={this.handleModeChange}
-                  type="radio"
-                  value={MODE_INLINE}
-                />
-                <label htmlFor="radio_inline">
-                  {`Inline Mode`}
-                </label>
-              </div>
-              <div className={classNames(`radio-item`, styles.radioItem)}>
-                <input
-                  checked={this.state.mode === MODE_REACT}
-                  id="radio_react"
-                  onChange={this.handleModeChange}
-                  type="radio"
-                  value={MODE_REACT}
-                />
-                <label htmlFor="radio_react">
-                  {`React Component`}
-                </label>
-              </div>
-            </div>
-          </div>
-          <div className={classNames(`example-code`, styles.exampleCode)}>
-            <ExampleCode accessToken={this.state.accessToken} toPersonEmail={this.state.toPersonEmail} type={this.state.mode} />
-          </div>
+          {`Altocloud Cisco Spark Guest Demo Example`}
           <button
             className={classNames(`button`, styles.button)}
             disabled={!loadButtonEnabled}
             onClick={this.handleSubmit}
           >
-            {`Load Widget`}
+            {`Login as Guest`}
           </button>
           <SparkOAuth
             clientId={this.state.clientId}
