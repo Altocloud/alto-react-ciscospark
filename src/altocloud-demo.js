c5e1c936b585de31c2f0259794613747b616d6e3
Colm Seale <colm.seale@altocloud.com>
2017-03-15 15:53:57 +0000

    added open, update and close of interaction

--- src/altocloud-demo.js
+++ src/altocloud-demo.js
@@ -5,9 +5,29 @@ import '@ciscospark/react-component-spark-fonts';
 
 import AltoSparkVideo from '@altocloud/alto-spark-video';
 
+const interaction = {};
+
+function openInteraction() {
+  console.log('interaction opened');
+}
+
+
+function closeInteraction() {
+  console.log('interaction closed');
+}
+
+
+function updateInteraction() {
+  console.log('interaction updated');
+}
+
 ReactDOM.render(
   <AltoSparkVideo
     account={`9c42eeeb`}
+    closeInteraction={closeInteraction}
+    interaction={interaction}
+    openInteraction={openInteraction}
+    updateInteraction={updateInteraction}
   />,
   document.getElementById(`main`)
 );
